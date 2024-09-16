856f9f38416cd76f8732cde01eb9493e8045489f
chhagan <chhaganswami1@gmail.com>
2024-09-16 22:34:26 +0530

    Finish

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,11 +5,11 @@ plugins {
 }
 
 android {
-    namespace = "com.cxzcodes.newappspin"
+    namespace = "com.pocket.rush"
     compileSdk = 34
 
     defaultConfig {
-        applicationId = "com.cxzcodes.newappspin"
+        applicationId = "com.pocket.rush"
         minSdk = 24
         targetSdk = 34
         versionCode = 1
@@ -47,6 +47,12 @@ dependencies {
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation("com.google.firebase:firebase-firestore:25.1.0")
     implementation("com.google.firebase:firebase-analytics:22.1.0")
+    implementation(platform("com.google.firebase:firebase-bom:33.2.0"))
+
+
+    implementation("com.google.firebase:firebase-auth")
+
+     implementation("com.google.android.gms:play-services-auth:21.2.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.2.1")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
